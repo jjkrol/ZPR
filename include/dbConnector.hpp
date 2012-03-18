0298d452cd29bed9c1a8ca2b1f30c03fffcf3888
Jacek Witkowski <jacek.witkowski@gmail.com>
2012-03-18 18:48:09 +0100

    DBConnector is now a Singleton

--- include/dbConnector.hpp
+++ include/dbConnector.hpp
@@ -1,44 +1,63 @@
 /**
- * @file DBConnector
+ * @file dbConnector.hpp
  * @brief File containing definition of an DB connectors
  * @author Jack Witkowski
  * @version 0.1
+ * Basicly there are 3 types of classes defined in this file:
+ * - DBConnector which is an interface for all singletons used
+ *   to communicate with diffrent types of databases (eg.SQLite DB)
+ * - DBConnectorFactory which is used to create DBConnectors
+ *   (it ensures that there is no more than one object of each type
+ *   of database connector
+ * - concrete versions of DBConnector (eg. SQLiteConnector)
 */
-
 #pragma once
 
 /**
- * @todo Sending queries to database
  * @todo Adding photos to database
  * @todo Compatibility check ?
 */
 
+class DBConnector;
+class SQLiteConnector;
+
+/*! @class DBConnectorFactory
+ *  Class used to provide instances of adequate concrete versions
+ *  of database connectors (eg. SQLiteConnector)
+*/
+class DBConnectorFactory {
+public:
+  static DBConnector* Instance(const char *type);
+};
+
+
 /*! @class DBConnector
  *  Interface providing functions for communication with databases.
 */
-
 class DBConnector {
-/*! @fn virtual int sendQuery(const char*) = 0;
- *  @brief For testing purposes.
- *  @warning Should be commented or removed in final version!
- *  @returns 0 if something went wrong
-*/
+public:
   virtual int sendQuery(const char *query) = 0;
+  /*! @fn virtual int sendQuery(const char*) = 0;
+   *  @brief For testing purposes.
+   *  @warning Should be commented or removed in final version!
+   *  @returns 0 if something went wrong
+  */
 };
 
-class SQLiteConnector {
+/*! @class SQLiteConnector
+  * @brief Concrete version of DBConnector. Uses SQLite Database.
+  *
+  * Must declare DBConnectorFactory as a friend. Must provide
+  * Instance function which gives access to the database
+  * (every concrete connector is implemented as a Singleton)
+*/
+class SQLiteConnector : public DBConnector{
+  friend class DBConnectorFactory;
+public:
   int sendQuery(const char *query);
+private:
+  static DBConnector *instance;
+  static DBConnector* Instance();
 };
-
-
-
-
-
-
-
-
-
-
-
 
 
--- src/dbConnector.cpp
+++ src/dbConnector.cpp
@@ -1,13 +1,53 @@
+/**
+ * @file dbConnector.cpp
+ * @brief File containing definition of an DB connectors
+ * @author Jack Witkowski
+ * @version 0.1
+*/
+
 #include <sqlite3.h>
 #include <iostream> //for testing only
 #include "../include/dbConnector.hpp"
 
-int SQLiteConnector::sendQuery(const char *query){
+//////////////////////////////////////////////////////////////////////
+//Definitions of DBConnectorFactory methods
+////////////////////////////////////////////////////////////////////////
+DBConnector* DBConnectorFactory::Instance(const char *type) {
+  /** @todo Concrete versions of DBConnector should
+    * invoke the Register() function to register their types
+    * in the register of types stored in the  DBConnectorGenerator
+    * object (the register should be static). Then when
+    * the DBConnectorFactory::Instance(type) is called,
+    * Instance() should look for the pointer to adequate Instance()
+    * function in its register and should call the function.
+  */
+  return SQLiteConnector::Instance();
+}
+
+
+//////////////////////////////////////////////////////////////////////
+//Definitions of SQLiteConnector methods
+////////////////////////////////////////////////////////////////////////
+/** @warning Don't forget to initialize 'instance' pointer to 0
+    in every concrete version of DBConnector
+*/
+
+
+/** Must be initialized with 0 before can be used*/
+DBConnector* SQLiteConnector::instance = 0;
+
+DBConnector* SQLiteConnector::Instance() {
+  if(instance == 0)
+    instance = new SQLiteConnector;  
+  return instance;
+}
+
+int SQLiteConnector::sendQuery(const char *query) {
   sqlite3 *database;
   sqlite3_open("Database.sqlite",&database);
   sqlite3_stmt *statement;
 
-  if(sqlite3_prepare_v2(database,query,-1,&statement,0)==SQLITE_OK){
+  if(sqlite3_prepare_v2(database,query,-1,&statement,0)==SQLITE_OK) {
     std::cout<<"WORKS!"<<std::endl;
     sqlite3_step(statement);
   }
@@ -15,11 +55,9 @@ int SQLiteConnector::sendQuery(const char *query){
   sqlite3_close(database);
 }
 
-
-
-
-//int main() {
-//  DBConnector *reference = SQLiteDBConnector::getInstance();
-//
-//  return 0;
-//}
+int main() {
+  //DBConnector *reference = SQLiteConnector::Instance();
+  DBConnector *sqlconnector = DBConnectorFactory::Instance("dupa");
+  sqlconnector->sendQuery("CREATE TABLE a (b INTEGER, c INTEGER);");
+  return 0;
+}
