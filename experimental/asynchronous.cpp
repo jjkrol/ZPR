8f8219823221be1344a9e72c62571d1c35e6d4ea
Jakub Jan Krol <jakub@jjkrol.pl>
2012-03-29 00:46:55 +0200

    Added - locks and conditions in the MessageQueue, concurrentQueue and Loop

--- experimental/asynchronous.cpp
+++ experimental/asynchronous.cpp
@@ -12,15 +12,13 @@
  *  passing pointers - which type is the best for passing return values?
  *  memory leaks (who will destroy passed data?)
  *  coding style - java style bitch!
- *
+ *  loose threads in concurrentLoop
  */
 
 // general classes
 
 class Ticket {
   public:
-    Ticket (){};
-    virtual ~Ticket (){};
     void * getPointer(){
       return pointer;
     };
@@ -29,21 +27,29 @@ class Ticket {
       pointer = newPointer;
     }
 
-    bool isReady(){
-      return ready;
+    void wait(){
+      boost::unique_lock<boost::mutex> lock(mut);
+      while(!ready){
+        cond.wait(lock);
+      }
     }
-    void setReady(bool readyState){
-      ready = readyState;
+
+    void setReady(){
+      boost::lock_guard<boost::mutex> lock(mut);
+      ready = true;
+      cond.notify_all();
     }
 
   private:
+    boost::mutex mut;
+    boost::condition_variable cond;
     bool ready;
-    void *  pointer;
+    void * pointer;
 };
 
 struct Message {
-    boost::function<void*()> functionObject;
-    Ticket * returnTicket;
+  boost::function<void*()> functionObject;
+  Ticket * returnTicket;
 };
 
 class MessageQueue {
@@ -56,7 +62,7 @@ class MessageQueue {
       inMsg.returnTicket = ticket; // loop has to know where to store the return value
 
       queueMutex.lock();
-        queue.push(inMsg);
+      queue.push(inMsg);
       queueMutex.unlock();
 
       return ticket;
@@ -66,8 +72,8 @@ class MessageQueue {
       Message retMsg;
 
       queueMutex.lock();
-        retMsg = queue.front();
-        queue.pop();
+      retMsg = queue.front();
+      queue.pop();
       queueMutex.unlock();
 
       return retMsg;
@@ -84,61 +90,142 @@ class MessageQueue {
 
 class Asynchronous{
   public:
-  Asynchronous(){
-    stopLoop = nietego;
-    mainLoopThread = boost::thread(&Asynchronous::mainLoop, this) ;
-  }
+    Asynchronous(){
+      stopLoops = nietego;
+      mainLoopThread = boost::thread(&Asynchronous::mainLoop, this) ;
+      concurrentLoopThread = boost::thread(&Asynchronous::concurrentLoop, this);
+    }
 
-  virtual ~Asynchronous(){
-      stopLoop = naturalnie;
+    virtual ~Asynchronous(){
+      stopLoops = naturalnie;
       mainLoopThread.join();
-  }
+      concurrentLoopThread.join();
+    }
 
   protected:
-
+    /*
+     * This loop is used for doing tasks from the queue
+     * one by one. 
+     *
+     */
     virtual void mainLoop(){
-      while(!stopLoop){  
+      while(!stopLoops)  
         if(!normalMessageQueue.isEmpty()){
           Message msg = normalMessageQueue.pop();
           void * output = msg.functionObject() ; 
           Ticket * ticket = msg.returnTicket;
           ticket->setPointer(output); 
-          ticket->setReady(naturalnie); // not really safe
+          ticket->setReady(); // not really safe
+
         }
-      }
     }
 
-    virtual void * invokeMethod(boost::function<void*()> funct){
-      //prepare a message
+    /* 
+     * this loop runs tasks, that can be
+     * run in parallel threads
+     */
+
+    virtual void concurrentLoop(){ 
+      boost::thread threadFromQueue;
+      while(!stopLoops)  
+        if(!concurrentMessageQueue.isEmpty()){
+          Message msg = concurrentMessageQueue.pop();
+          threadFromQueue = boost::thread(&Asynchronous::invokeConcurrent, this, msg);
+          //TODO what happens with those threads?
+        }
+    }
+
+    void invokeConcurrent(Message msg){
+      void * output = msg.functionObject() ; 
+      Ticket * ticket = msg.returnTicket;
+
+      ticket->setPointer(output); 
+      ticket->setReady();
+    }
+
+    virtual void * invokeQueuedMethod(boost::function<void*()> funct){
+/*      //prepare a message
       Message msg;
       msg.functionObject = funct;
-      
+
       //prepare return ticket
       Ticket * ticket;
       ticket = normalMessageQueue.push(msg);
 
       //wait for the order to be complete
-      while(!ticket->isReady())
-        ;
+      ticket->wait();
       return ticket->getPointer();
+*/ 
+      return invokeMethod(funct, normalMessageQueue);
+    }
+
+    virtual void * invokeConcurrentMethod(boost::function<void*()> funct){
+/*      //prepare a message
+      Message msg;
+      msg.functionObject = funct;
+
+      //prepare return ticket
+      Ticket * ticket;
+      ticket = concurrentMessageQueue.push(msg);
+
+      //wait for the order to be complete
+      ticket->wait();
+      return ticket->getPointer();
+ */
+      return invokeMethod(funct, concurrentMessageQueue);
+    }
+  
+    virtual void * invokeMethod(boost::function<void*()> funct, MessageQueue& mq){
+      //prepare a message
+      Message msg;
+      msg.functionObject = funct;
+
+      //prepare return ticket
+      Ticket * ticket;
+      ticket = mq.push(msg);
+
+      //wait for the order to be complete
+      ticket->wait();
+      void * pointer = ticket->getPointer();
+      return pointer;
+
     }
 
     MessageQueue normalMessageQueue;
-    bool stopLoop;
+    MessageQueue concurrentMessageQueue;
+    bool stopLoops;
     boost::thread mainLoopThread;
+    boost::thread concurrentLoopThread;
 };
 
 // specific classes
 
 class PizzaDeliverySystem : public Asynchronous {
   public:
+    PizzaDeliverySystem(){
+      pizzaHouseName = "Kotek";
+    }
 
-    /* all public methods are similar to that pattern:
+    /*
+     * for simple public methods
+     * that do not change anything
+     * and do not depend on external objects either
+     * we can leave the thread thing alone
+     * and just return the value:
+     */
+    std::string getPizzaHouseName(){
+      return pizzaHouseName; 
+    }
+
+    /* 
+     * public methods that use other objects and/or change something
+     * are similar to that pattern:
+     *
      * returntype methodName(arguments){
      *  return *reinterpret_cast<returntype*>( if we return by value
      *  OR
      *  return reinterpret_cast<returntype*>( if we return pointer
-     *    invokeMethod(
+     *    invokeQueuedMethod(
      *      boost::bind(
      *        &ObjectName::internalMethodName, this, arguments
      *        )
@@ -146,16 +233,27 @@ class PizzaDeliverySystem : public Asynchronous {
      *    );
      * }
      */
-
     std::string getPizza(std::string pizzaName){
       return *reinterpret_cast<std::string*>(
-          invokeMethod(
+          invokeQueuedMethod(
             boost::bind(
               &PizzaDeliverySystem::internalGetPizza, this, pizzaName
               )
             )
           );
     }
+    /*
+     * for public methods that do not change anything but may tak a while
+     * (for example certain database queries, disk operations)
+     * we can use the concurrent queue, which runs all messages
+     * in parallel threads
+     * pattern is similar to the previous one, the only difference
+     * is that we use invokeConcurrentMethod instead of invokeQueuedMethod
+     * try it and see what happens
+     * 
+     *
+     *
+     */
 
   private:
 
@@ -168,12 +266,17 @@ class PizzaDeliverySystem : public Asynchronous {
 
     void* internalGetPizza(std::string pizzaName){
       std::cout<<"-- Chef: I'm making a delicious "<<pizzaName<<" pizza..."<<std::endl;
-      sleep(3);
-
+      if(pizzaName == "peperoni"){ // show the difference between concurrent and queued message queue
+      sleep(5);
+      }
+      else{
+sleep(1);
+      }
       std::string * pizza = new std::string(pizzaName+" and green peppers, mushrooms, olives, chives");
-
       return reinterpret_cast<void*>(pizza);
     }
+
+    std::string pizzaHouseName;
 };
 
 class MeInMyHouse {
@@ -204,13 +307,7 @@ class MeInMyHouse {
       sleep(1);
 
       pizzaOrder1.join();
-      std::cout<<"Got my first pizza, it has: "<<pizza1<<std::endl;
-      sleep(1);
-
       pizzaOrder2.join();
-      std::cout<<"Got my second pizza, it has: "<<pizza2<<std::endl;
-
-      sleep(1);
 
       std::cout<<"Delicious!"<<std::endl;
     }
@@ -221,18 +318,21 @@ class MeInMyHouse {
 
     void getPizza1(){
       pizza1 = pizzaGuys.getPizza("peperoni");
+      std::cout<<"Got my first pizza, it has: "<<pizza1<<std::endl;
     }
 
     void getPizza2(){
       pizza2 = pizzaGuys.getPizza("pineapple");
+      std::cout<<"Got my second pizza, it has: "<<pizza2<<std::endl;
     }
 };
 
 
 int main(int argc, char** argv){
   MeInMyHouse me;
-  std::cout<<"I'm at home, hungry as fuck. Let's order some pizza!"<<std::endl;
+  std::cout<<"I'm at home, hungry. Let's order some pizza!"<<std::endl;
   sleep(2);
 
   me.letsEatPizza();
+
 }
