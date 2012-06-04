a3d8250cd55713a863f5c9fab4067ac2dca1ce19
Maciej Suchecki <mc.suchecki@gmail.com>
2012-06-04 03:53:47 +0200

    Changed - core.hpp structure and documentation

--- include/core.hpp
+++ include/core.hpp
@@ -19,189 +19,84 @@ class PluginManager;
 class UserInterface;
 class ConfigurationManager;
 
-typedef std::vector<Photo*> photos_t;
-
 /**
  * @class CoreController
- * @brief A singleton class acting as a main application controller
+ * @brief A singleton class acting as a main application controller.
  */
 class CoreController {
-
   public:
-
-    static CoreController* getInstance(std::string forcedConfigPath="");
-    /**
-     * @brief destroys the CoreController object, but joins all the threads before
-     *
-     */
+    static CoreController* getInstance(std::string forcedConfigPath = "");
+    void startApplication(int argc = 0, char ** argv = NULL);
     void destroy();
-    /**
-     * @brief runs the gui, all the preparations are done in constructor by now
-     */
-    void startApplication(int argc=0, char ** argv=NULL);
 
-    /**
-     * @TODO implement
-     */
-    bool hasLibraryPathSet();
-
-    /**
-     * sets the library path
-     */ 
-    void setLibraryPath(boost::filesystem::path libraryPath);
-
-    /**
-     * @returns an object representing the directory tree 
-     */
-    Glib::RefPtr<Gtk::TreeStore> getDirectoryTree();
-
-    /**
-     * @returns an object representing the directory tree for DB manager
-     */
-    Glib::RefPtr<Gtk::TreeStore> getFilesystemTree();
-
-    void expandDirectory(const Gtk::TreeModel::iterator &iter,
-        const Gtk::TreeModel::Path &path);
-
-    bool hasPhotos(const boost::filesystem::path& directoryPath);
-
-    /**
-     * @returns data of the photos in a given directory
-     */
-    std::vector<PhotoData> getPhotos(boost::filesystem::path directoryPath);
-
-    /**
-     * @returns data of the thumbnails in a given directory
-     */
+    //methods associated with photos loading
+    bool isCurrentPhotoSet();
+    PhotoData getCurrentPhoto();
+    PhotoData getNextPhoto();
+    PhotoData getPreviousPhoto();
+    void setCurrentPhoto(boost::filesystem::path photoPath);
+    void setCurrentDirectory(boost::filesystem::path directoryPath);
     std::vector<PhotoData> getThumbnails(boost::filesystem::path directoryPath);
 
-    /**
-     * sets current photo, should setCurrentDirectory or setCurrentTagSet first
-     */
-    void setCurrentPhoto(boost::filesystem::path photoPath);
-
-    /**
-     * sets current directory
-     */
-    void setCurrentDirectory(boost::filesystem::path directoryPath);
-
-    /**
-     * sets current tag set, from which next and prev photos are taken
-     */
-    void setCurrentTagSet(const std::set<std::string> &tagSet);
-
-    /**
-     * @returns data of the current photo
-     */
-    PhotoData getCurrentPhoto();
-
-    /**
-     * @returns next photo and sets it as a current photo
-     */
-    PhotoData getNextPhoto();
-
-    /**
-     * @returns previous photo and sets it as the current photo
-     */
-    PhotoData getPreviousPhoto();
-
-
-    /**
-     * @returns A tree of key-value pairs extracted from the configuration file.
-     */
-    boost::property_tree::ptree getConfiguration();
-
-    /**
-     * @brief Saves configuration stored in a property tree
-     */
-    void putConfiguration(boost::property_tree::ptree config);
-
-    /**
-     * @warning Should be used carefully! Access to photos and directories should be
-     * provided by the Disk class.
-     * @returns Absolute path to the LibraryDirectory
-     */
-    boost::filesystem::path getLibraryDirectoryPath();
-
-    /**
-     * @returns all tags
-     */
-    Glib::RefPtr<Gtk::ListStore> getTagsList();
-
-    void addTagToActivePhoto(std::string tag);
-    void removeTagFromActivePhoto(std::string tag);
-    Glib::RefPtr<Gtk::ListStore> getTagsOfActivePhoto();
-
-    void addFolderToDB(const Gtk::TreeModel::iterator &);
-    void removeFolderFromDB(const Gtk::TreeModel::iterator &);
-
+    //methods associated with database
+    bool hasLibraryPathSet();
     void sendChangesToDB();
     void cancelDBChanges();
+    void addFolderToDB(const Gtk::TreeModel::iterator &);
+    void removeFolderFromDB(const Gtk::TreeModel::iterator &);
+    void expandDirectory(const Gtk::TreeModel::iterator &iter,
+        const Gtk::TreeModel::Path &path);
+    bool hasPhotos(const boost::filesystem::path& directoryPath);
+    Glib::RefPtr<Gtk::TreeStore> getDirectoryTree();
+    Glib::RefPtr<Gtk::TreeStore> getFilesystemTree();
 
-    void setExternalEditor(std::string name);
+    //methods associated with tags
+    Glib::RefPtr<Gtk::ListStore> getTagsList();
+    Glib::RefPtr<Gtk::ListStore> getTagsOfActivePhoto();
+    void setCurrentTagSet(const std::set<std::string> &tagSet);
+    void addTagToActivePhoto(std::string tag);
+    void removeTagFromActivePhoto(std::string tag);
 
-    /**
-     * Invoke gimp to edit photo
-     */
-    void editInExternalEditor();
-
-    /**
-     * Apply selected effect
-     */
-
-    void applyEffectOfSelectedPlugin();
-
-    /**
-     * @returns a widget for setting plugin parameters
-     */
-    Gtk::Widget * getPluginBox(std::string name);
-
-    /**
-     * @returns names of all available plugins
-     */
-    std::vector<std::string> getPluginNames();
-
-    /**
-     * saves all modified photos
-     */
+    //methods associated with plugins and effects
     void savePhotos();
-
     void saveCurrentPhoto();
-    /**
-     * @returns true if there are photos modified, which are not saved
-     */
     bool modifiedPhotosExist();
+    void editInExternalEditor();
+    void applyEffectOfSelectedPlugin();
+    std::vector<std::string> getPluginNames();
+    Gtk::Widget* getPluginBox(std::string name);
 
-    bool isCurrentPhotoSet();
+    //methods associated with configuration
+    void setExternalEditor(std::string name);
+    boost::filesystem::path getLibraryDirectoryPath();
+    void setLibraryPath(boost::filesystem::path libraryPath);
+    void putConfiguration(boost::property_tree::ptree config);
 
   private:
-    CoreController (std::string forcedConfigPath="");
-    CoreController& operator= (const CoreController&);
-    CoreController (const CoreController&);
-    ~CoreController (){};
+    //hidden constructors and descructor
+    CoreController(std::string forcedConfigPath = "");
+    CoreController& operator=(const CoreController&);
+    CoreController(const CoreController&);
+    ~CoreController() {};
 
-    Plugin * selectedPlugin;
+    //other private methods
     void manageConfig(std::string);
     void manageDisk();
     void manageDatabase();
-    void doSomeLongLastingTask();
-
-    /**
-     * Method responsible for adding subdirectories to directory tree.
-     *@param dir Directory in which we look for subdirectories.
-     *@param row Tree row to which we append new children.
-     *@param depth How deep should the method go. Set -1 for going deepest
-     */
     void addSubdirectories(Directory *, const Gtk::TreeModel::Row &, int);
     void addAbsoluteSubdirectories(Directory *, const Gtk::TreeModel::Row &, int);
+    std::vector<PhotoData> getPhotos(boost::filesystem::path directoryPath);
 
     static CoreController* instance;
 
-    std::vector<Photo*>::iterator currentPhoto;
+    //variables representing current state of application
+    Plugin *selectedPlugin;
     Directory* currentDirectory;
+    std::vector<Photo*>::iterator currentPhoto;
     std::vector<Photo*> currentPhotoSet;
     std::set<Photo*> modifiedPhotos;
 
+    //connections with other classes
     DBConnector* db;
     UserInterface *gui;
     ConfigurationManager* configManager;
@@ -220,6 +115,4 @@ class CoreController {
     //containers for comminucating with DBManager
     std::vector<Gtk::TreeModel::iterator> added_folders;
     std::vector<Gtk::TreeModel::iterator> deleted_folders;
-
-
 };
